274796fdedabc28b8410f00a5becf1d13c41b6cb
Renasyaa <renasyamalkahaq@gmail.com>
2024-02-28 13:36:11 +0700

    first commit

---
+++ lib/crud_home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeCrud extends StatelessWidget {
+  const HomeCrud({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('CRUD'),
+      ),
+      body: Container(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:renasyaa_fb/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
